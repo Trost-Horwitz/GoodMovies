a9fe0c2ed7eaf7d75980611a150b739310967653
Alexander Trost <AlexRTrost@gmail.com>
2018-08-07 18:48:57 -0400

    Add MovieCardRow component

---
+++ src/components/MovieCardRow/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import MovieCardItem from "../MovieCardItem";
+
+const Container = styled.section`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+`;
+
+function MovieCardRow(props) {
+  return (
+    <Container>
+      {props.movies && props.movies.map(movie => <MovieCardItem {...movie} />)}
+    </Container>
+  );
+}
+
+export default MovieCardRow;
