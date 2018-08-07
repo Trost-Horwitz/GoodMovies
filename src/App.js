462f92e6b66fb848b004ebed90a3fd8e7bf5bedf
Alexander Trost <AlexRTrost@gmail.com>
2018-08-07 18:52:56 -0400

    Add NavBar and Movie Cards

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import movieFetch from './adapters/movieFetch'
+import React, { Component } from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 
+import NavBar from "./components/NavBar";
+import MovieCardRow from "./components/MovieCardRow";
+
+import movieFetch from "./adapters/movieFetch";
+import theme from "./theme";
 class App extends Component {
-
-  constructor(){
-    super()
-
-    this.state = {
-    }
+  constructor() {
+    super();
+    this.state = {};
   }
 
-  componentDidMount(){
-
+  componentDidMount() {
     //Example fetch for popular movies -- see adapters/movieFetch.js for all options
-    movieFetch.popularMovies().then(json=>(this.setState({apiData:json})))
-
+    movieFetch.popularMovies().then(json => this.setState({ apiData: json }));
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        {// this renders movie titles on the page as an example
-          this.state.apiData && this.state.apiData[0].title ? this.state.apiData.map(mov=>(<h2>{mov.title}</h2>)) : <h2></h2>}
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <NavBar />
+          <MovieCardRow movies={this.state.apiData} />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
