73d4dd864feb73d6f18e5d257433eba998300f28
Alexander Trost <AlexRTrost@gmail.com>
2018-08-08 17:50:19 -0400

    bug: Set variable composeEnhancers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import reducer from "../reducers/reducer";
 import thunk from "redux-thunk";
 
-composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
   const store = createStore(
