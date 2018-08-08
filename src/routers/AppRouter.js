8d2e13edfa3365c3feb5d454b416831959b4db0e
Alexander Trost <AlexRTrost@gmail.com>
2018-08-08 07:37:40 -0400

    Add AppRouter

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,25 @@
+// This file is mainly redux-router and allows for navigation through the components without the need to update too much.
+// Using this sort of file we can keep our files small and nice and clean. Getting this done in <30 lines is great.
+
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import HomePage from "../components/HomePage";
+import NotFoundPage from "../components/NotFoundPage";
+
+const AppRouter = props => (
+  <BrowserRouter>
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={() => <HomePage {...props} />} />
+        {/* <Route path="/movie/:id" component={MovieViewPage} /> */}
+        {/* <Route path="/login" component={LoginPage} /> */}
+        {/* <Route path="/logout" component={LogOut} /> */}
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
