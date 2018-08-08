74ef6742e5d28711e5f15a809d03fd83e7911397
Alexander Trost <AlexRTrost@gmail.com>
2018-08-08 17:49:53 -0400

    Add redux

--- src/components/FirebaseAuth/SignInScreen.js
+++ src/components/FirebaseAuth/SignInScreen.js
@@ -1,33 +1,45 @@
 // Import FirebaseAuth and firebase.
-import React from 'react';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase';
+import React from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+import { connect } from "react-redux";
 
-
-
-// Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/user',
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ]
-};
+import { startLogin } from "../../actions/actions";
 
 class SignInScreen extends React.Component {
-  render() {
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: "popup",
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    signInSuccessUrl: "/user",
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: authDetails =>
+        this.props.dispatch(startLogin(authDetails))
+    }
+  };
+
+  render(props) {
     return (
       <div>
-        <h1>My App</h1>
+        <h1>Movie Notes</h1>
         <p>Please sign-in:</p>
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+        <StyledFirebaseAuth
+          uiConfig={this.uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
       </div>
     );
   }
 }
 
-export default SignInScreen
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(mapStateToProps)(SignInScreen);
