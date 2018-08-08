ca4dbd7e6b6ec053283911e97419b726fb5e1a99
Alexander Trost <AlexRTrost@gmail.com>
2018-08-08 17:49:34 -0400

    Add GetUser function - wip

--- src/adapters/firebaseFetch.js
+++ src/adapters/firebaseFetch.js
@@ -1,32 +1,43 @@
-import firebase from '../firebase/firebase'
-
+import firebase from "../firebase/firebase";
 
 const firebaseFetch = {
   db: firebase.firestore(),
   //creates new user in DB with empty to_watch list
-  createUser: function(name){
-    return this.db.collection("users").doc(`${name}`).set({
-      username:`${name}`,
-      toWatch_list:[]
-    }).catch(error=>console.error("Error creating new user in firebase:", error))
+  createUser: function(name) {
+    return this.db
+      .collection("users")
+      .doc(`${name}`)
+      .set({
+        username: `${name}`,
+        toWatch_list: []
+      })
+      .catch(error =>
+        console.error("Error creating new user in firebase:", error)
+      );
   },
 
   // returns array of objects containing user info
-  getAllUsers: function(){
-    return this.db.collection("users").get()
-    .then(function(querySnapshot){
-      let users = []
-      querySnapshot.forEach(function(doc) {
-        users.push({...doc.data(), id:doc.id})
-      })
-      return users
-    })
+  getAllUsers: function() {
+    return this.db
+      .collection("users")
+      .get()
+      .then(function(querySnapshot) {
+        let users = [];
+        querySnapshot.forEach(function(doc) {
+          users.push({ ...doc.data(), id: doc.id });
+        });
+        return users;
+      });
+  },
+
+  getUser: function(uid) {
+    console.log({ uid });
+    return uid;
   }
 
   // multiple selection
 
   // db.collection("users").where("username", "==", "secondUser").get().then(querySnapshot=>(querySnapshot.forEach(doc=>console.log(doc.data()))))
-
 
   // clears error about timestamp
 
@@ -35,6 +46,6 @@ const firebaseFetch = {
   // }
   //
   // firestore.settings(this.settings);
-}
+};
 
-export default firebaseFetch
+export default firebaseFetch;
