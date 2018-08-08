2392e4cc75e7ab5cf496b76b2f94561b087d303e
Alexander Trost <AlexRTrost@gmail.com>
2018-08-08 15:12:57 -0400

    Add redux store with thunk

---
+++ src/store/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import reducer from "../reducers/reducer";
+import thunk from "redux-thunk";
+
+composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => {
+  const store = createStore(
+    combineReducers({
+      reducer
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+  );
+  return store;
+};
