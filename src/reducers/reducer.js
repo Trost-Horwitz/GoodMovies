ae7af6bee553b748b7207f763a1ac6404fc73b5d
Alexander Trost <AlexRTrost@gmail.com>
2018-08-09 08:27:54 -0400

    Add get_user_data reducer - wip

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -4,6 +4,8 @@ export default (state = reducerDefaultState, action) => {
   switch (action.type) {
     case "ADD_MOVIE":
       return [...state, action.payload];
+    case "GET_USER_DATA":
+      return [...state, action.payload];
     default:
       return state;
   }
