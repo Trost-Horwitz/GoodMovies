5d57871ca67ac9169b6a6f9c81440183f94d9839
Alexander Trost <AlexRTrost@gmail.com>
2018-08-10 09:54:08 -0400

    Change toWatchList from array to map and create add/remove functions

--- src/adapters/firebaseFetch.js
+++ src/adapters/firebaseFetch.js
@@ -1,45 +1,76 @@
-import firebase from '../firebase/firebase'
-
+import firebase from "../firebase/firebase";
 
 const firebaseFetch = {
   db: firebase.firestore(),
   //creates new user in DB with empty to_watch list
-  createUser: function(uid){
-    return this.db.collection("users").doc(`${uid}`).set({
-      toWatch_list:[]
-    }).catch(error=>console.error("Error creating new user in firebase:", error))
+  createUser: function(uid) {
+    return this.db
+      .collection("users")
+      .doc(`${uid}`)
+      .set({
+        toWatchList: {}
+      })
+      .catch(error =>
+        console.error("Error creating new user in firebase:", error)
+      );
   },
 
   // returns array of objects containing user info
-  getAllUsers: function(){
-    return this.db.collection("users").get()
-    .then(function(querySnapshot){
-      let users = []
-      querySnapshot.forEach(function(doc) {
-        users.push({...doc.data(), id:doc.id})
-      })
-      return users
-    })
+  getAllUsers: function() {
+    return this.db
+      .collection("users")
+      .get()
+      .then(function(querySnapshot) {
+        let users = [];
+        querySnapshot.forEach(function(doc) {
+          users.push({ ...doc.data(), id: doc.id });
+        });
+        return users;
+      });
   },
 
-  getUserData: function(uid){
-    return this.db.collection("users").doc(`${uid}`).get()
-    .then(doc => {
-      if (doc.exists) {
-        console.log("Document data:", doc.data());
-        return doc.data()
-      } else {
-        console.log("No such document!");
-        return null
-      }
-    })
+  getUserData: async function(uid) {
+    try {
+      const userRef = await this.db.collection("users").doc(uid);
+      const userObj = await userRef.get();
+      return await userObj.data();
+    } catch (err) {
+      console.log("User Data not found for ID:", uid, "Error", err);
+      return null;
+    }
   },
 
+  addMovieToList: async function(uid, movieObj) {
+    try {
+      const toWatchWithID = `toWatchList.${movieObj.id}`;
+      const userRef = await this.db.collection("users").doc(uid);
+      await userRef.update({
+        [toWatchWithID]: movieObj
+      });
+      return movieObj; // Not sure what this function should return
+    } catch (err) {
+      console.log("User Data not found for ID:", uid, "Error", err);
+      return null;
+    }
+  },
+
+  removeMovieFromList: async function(uid, movieObj) {
+    try {
+      const toWatchWithID = `toWatchList.${movieObj.id}`;
+      const userRef = await this.db.collection("users").doc(uid);
+      const removeMovie = await userRef.update({
+        [toWatchWithID]: firebase.firestore.FieldValue.delete()
+      });
+      return movieObj;
+    } catch (err) {
+      console.log("User Data not found for ID:", uid, "Error", err);
+      return null;
+    }
+  }
 
   // multiple selection
 
   // db.collection("users").where("username", "==", "secondUser").get().then(querySnapshot=>(querySnapshot.forEach(doc=>console.log(doc.data()))))
-
 
   // clears error about timestamp
 
@@ -48,6 +79,6 @@ const firebaseFetch = {
   // }
   //
   // firestore.settings(this.settings);
-}
+};
 
-export default firebaseFetch
+export default firebaseFetch;
