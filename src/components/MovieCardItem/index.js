16c60bb88e6c886ab0703e422d152911b4c39ed1
Alexander Trost <AlexRTrost@gmail.com>
2018-08-12 12:58:37 -0400

    Remove movie overview to allow for cleaner card interface

--- src/components/MovieCardItem/index.js
+++ src/components/MovieCardItem/index.js
@@ -8,18 +8,24 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "../Button";
 import AddToListButton from "../AddToListButton";
 import Typography from "@material-ui/core/Typography";
+import styled from "styled-components";
 
 import firebase from "firebase";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+const CardContainer = styled.div`
+  padding: 0.5rem 0;
+`;
+
 const styles = {
   card: {
-    maxWidth: 345
+    width: 275,
+    height: "100%"
   },
   media: {
     height: 0,
-    paddingTop: "56.25%" // 16:9
+    paddingTop: "135%"
   }
 };
 
@@ -31,42 +37,25 @@ const handleClickAddMovie = props => {
 function MovieCardItem(props) {
   const { movie, classes } = props;
   return (
-    <div>
+    <CardContainer>
       <Card className={classes.card}>
-        <CardMedia
-          className={classes.media}
-          image={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-          title={movie.title}
-        />
-        <CardContent>
-          <Link to={{ pathname: `/movie/${movie.id}` }}>
-            <Typography gutterBottom variant="headline" component="h2">
+        <Link to={{ pathname: `/movie/${movie.id}` }}>
+          <CardMedia
+            className={classes.media}
+            image={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            title={movie.title}
+          />
+          <CardContent>
+            <Typography variant="subheading" component="h6">
               {movie.title}
             </Typography>
-          </Link>
-          <Typography component="p">
-            {movie.overview.substring(0, 250)}
-            {movie.overview.length > 250 ? "..." : ""}
-          </Typography>
-        </CardContent>
+          </CardContent>
+        </Link>
         <CardActions>
-          <Link to={{ pathname: `/movie/${movie.id}` }}>
-            <Button size="small" color="primary">
-              View Details
-            </Button>
-          </Link>
-          {firebase.auth().currentUser ? (
-            <AddToListButton movie={movie} />
-          ) : (
-            <Link to="/signin">
-              <Button size="small" color="primary">
-                Login & Add To Watch List
-              </Button>
-            </Link>
-          )}
+          <AddToListButton movie={movie} />
         </CardActions>
       </Card>
-    </div>
+    </CardContainer>
   );
 }
 
