6742c06d5587ede42a5090e95d04df99be79a03e
SuperJesseH <therealjhorwitz@gmail.com>
2018-08-13 16:21:25 -0400

    added search/filter to user page

--- src/components/UserPage/index.js
+++ src/components/UserPage/index.js
@@ -4,9 +4,48 @@ import firebase from "firebase";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import MovieCardRow from "../MovieCardRow";
+import TextField from '@material-ui/core/TextField';
 
 class UserPage extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+
+  componentDidMount(){
+    const obj = this.props
+
+    console.log(obj)
+  }
+
+  componentDidUpdate(){
+    if (!this.state.userMovies){
+      this.setState({...this.state,userMovies:Object.values(this.props.reducer.toWatch)})
+    }
+  }
+
+
+  handleText = (e) =>{
+
+    console.log(Object.values(this.props.reducer.toWatch))
+
+    if (e.target.value !== "" && this.state.userMovies){
+
+      const filteredMovies =  Object.values(this.props.reducer.toWatch).filter(movie => movie.title.toLowerCase().includes(`${e.target.value.toLowerCase()}`))
+
+      this.setState({userMovies:filteredMovies})
+    } else {
+      this.setState({...this.state,userMovies:Object.values(this.props.reducer.toWatch)})
+    }
+  }
+
+
+
+
   render(){
+    console.log(this.state)
     //authDetails has these useful properties:
     // authDetails.additionalUserInfo.isNewUser (boolean)
     // authDetails.user.uid (string)
@@ -22,7 +61,11 @@ class UserPage extends React.Component{
       <h2>Hello {firebase.auth().currentUser.displayName}!</h2>
       <h3>Your Movies To Watch:</h3>
       {Object.keys(this.props.reducer.toWatch).length !== 0 ?
-         <MovieCardRow movies={Object.values(this.props.reducer.toWatch)} />
+        <React.Fragment>
+          <TextField autoFocus id="full-width" label="Search Your Movies By Title" InputLabelProps={{ shrink: true, }} onKeyUp={this.handleText} placeholder="Search..." fullWidth margin="normal"/>
+
+         <MovieCardRow movies={this.state.userMovies} />
+       </React.Fragment>
         : <p>Add Movies To Your List!</p>
 
       }
@@ -34,7 +77,6 @@ class UserPage extends React.Component{
     );
   }
 };
-
 
 
 const mapStateToProps = state => {
