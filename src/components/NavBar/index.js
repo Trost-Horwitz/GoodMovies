07f2ad86b2a4c015cc46b2b25169ff480329ea95
Alexander Trost <AlexRTrost@gmail.com>
2018-08-18 13:29:37 -0400

    Remove unused NavBar button "Browse

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -47,7 +47,7 @@ function ButtonAppBar(props) {
             <NavLink to="/search">
               <TextButton size="large">Search</TextButton>
             </NavLink>
-            <TextButton size="large">Browse</TextButton>
+            {/* <TextButton size="large">Browse</TextButton> */}
 
             {/* consider making one button "sign in / sign up" since firebase auth takes care of both in the same UI. Once signin === true the button can change to logout. */}
 
